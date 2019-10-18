121d08969258327cea07086aa122cc6045532ae7
Vaishnavi <havavaishnavi2810@gmail.com>
2019-10-18 16:29:13 +0530

    project

--- app/src/main/java/chandan/prasad/androidsliderapplication/MainActivity.java
+++ app/src/main/java/chandan/prasad/androidsliderapplication/MainActivity.java
@@ -1,26 +1,67 @@
 package chandan.prasad.androidsliderapplication;
 
+import androidx.cardview.widget.CardView;
+
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button admin,user;
+    CardView cardView, cardView1, cardView2, cardView3, cardView4, cardView5;
+    Button button;
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        admin=findViewById(R.id.btnAdmin);
-        user=findViewById(R.id.btnUser);
 
-        admin.setOnClickListener(new View.OnClickListener() {
+        cardView = findViewById(R.id.cardViewA);
+        cardView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                //Intent intent =new Intent(getApplicationContext(),AdminActivity.class);
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Start.class);
+                startActivity(intent);
+            }
+        });
+        cardView1 = findViewById(R.id.cardViewB);
+        cardView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Start.class);
+                startActivity(intent);
+            }
+        });
+
+        cardView2 = findViewById(R.id.cardViewC);
+        cardView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Start.class);
+                startActivity(intent);
+            }
+        });
+
+        cardView3 = findViewById(R.id.cardViewD);
+        cardView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Start.class);
+                startActivity(intent);
+            }
+        });
+
+        cardView4 = findViewById(R.id.cardViewE);
+        cardView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Start.class);
+                startActivity(intent);
             }
         });
     }
